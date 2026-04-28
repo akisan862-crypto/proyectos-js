6feddd3228b5ae1df7e01c0a24be623ff5fb2634
Ash <akisan862@gmail.com>
2026-04-27 18:35:27 -0600

    Tarea de tipos de datos terminada

---
+++ tipos-de-dato.js
@@ -0,0 +1,30 @@
+console.log(typeof 42);             
+console.log(typeof 'Veinticinco');  
+console.log(typeof -666);          
+console.log(typeof true);           
+console.log(typeof 0);              
+console.log(typeof '');             
+console.log(typeof null);           
+console.log(typeof undefined);      
+
+
+
+console.log(typeof FALSE);        
+
+
+
+console.log("\n--- Mis propios ejemplos ---");
+
+
+console.log(typeof 3.1416);        
+
+
+console.log(typeof [1, 2, 3]);     
+
+console.log(typeof { nombre: "Juan", edad: 20 }); 
+
+
+console.log(typeof function() {});  
+
+
+console.log(typeof NaN);
